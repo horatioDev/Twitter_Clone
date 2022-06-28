309a22e042f776a978b2bf42c0ef1a9d19688f09
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-06-27 20:59:09 -0400

    added templates and layout

--- twitter_clone/tweets/views.py
+++ twitter_clone/tweets/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+from .models import Post
 
 from django.http import HttpResponse
 # Create your views here.
 
 
-def index(render):
-    return HttpResponse('Home')
+def index(request):
+    # Get all posts; limit: 20
+    posts = Post.objects.all()[:20]
+    return render(request, 'posts.html')
 
 def edit(render):
     return HttpResponse('Edit')
