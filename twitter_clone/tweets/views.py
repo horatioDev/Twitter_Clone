e026cf31cac1cf67dde514e9380b1b7b83120a47
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-06-29 14:47:15 -0400

    modified files with tutor to complete edit functionality

--- twitter_clone/tweets/views.py
+++ twitter_clone/tweets/views.py
@@ -7,57 +7,61 @@ from .forms import PostForm, EditPostForm
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
-
+# Define a 'Post' view:
 def index(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
+        # Check if valid:
         if form.is_valid():
+            # Save to database:
             form.save()
+            # Redirect to same/current page:
             return HttpResponseRedirect('/')
         else:
             return HttpResponseRedirect(form.errors.as_json())
-
     # Get all posts; limit: 20
     posts = Post.objects.all()[:20]
     return render(request, 'posts.html', {'posts':posts})
 
-
+# Define a 'Edit' view:
 def edit(request, post_id):
+    # Create a instance to hold data:
+    post = Post.objects.get(id=post_id)
+    # Check if method is 'POST':
     if request.method == 'POST':
-        form = EditPostForm(request.POST, post_id)
+        form = EditPostForm(request.POST, request.FILES, instance=post)
+        # Check if valid:
         if form.is_valid():
+            # Save to database:
             form.save()
-            return HttpResponseRedirect('edit')
-        else:
-            return HttpResponseRedirect(form.errors.as_json())
-
-    # Get user post
-    post = Post.objects.get(id = post_id)    
-    context = {'post':post}
-    return render(request, 'edit.html', context)
-
-# Define a like function
-def like(request, post_id):
-    likes = Post.objects.get('like_count')
-    
-
-# Define update function
-def update(request, post_id):
-    if request.method == 'POST':
-        form = EditPostForm(request.POST, post_id)
-        if form.is_valid():
-            form.save()
+            # Redirect to same/current page:
             return HttpResponseRedirect('/')
         else:
-            return HttpResponseRedirect(form.errors.as_json())
+            return HttpResponse("Not Valid")
+            # return HttpResponseRedirect(form.errors.as_json())
+    # Display user edited post
+    return render(request, 'edit.html', {'post':post})
 
-    # Get user post
-    post = EditPostForm(request.POST, post_id)
-    post.save()    
-    context = {'post':post}
-    return render(request, 'posts.html', context)
+# Define 'Like' function
+def like(request, post_id):
+    # Create a post instance to hold data:
+    likes = Post.objects.get(id=post_id)
+    # Increment likes:
+    likes += 1
+    # Save likes:
+    likes.save()
+    # Redirect to same/current page:
+    return HttpResponseRedirect('/')
 
-# Define delete function
+# Define 'Cancel' function
+def cancel(request, post_id):
+    # Find user post
+    post = Post.objects.get(id = post_id)
+    # Delete user post
+    post.save()
+    return HttpResponseRedirect('index')
+
+# Define 'Delete' function
 def delete(request, post_id):
     # Find user post
     post = Post.objects.get(id = post_id)
